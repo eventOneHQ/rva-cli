f8e22d1c83b34986cad9c1e1fdfe1801861d4e5e
Noah Prail <nprail@filiosoft.com>
2018-01-24 13:06:34 -0500

    Further deploy improvements.

--- scripts/move.sh
+++ scripts/move.sh
@@ -1,13 +1,14 @@
 APP_NAME=rva-cli
+BIN_PATH=build/${APP_NAME}
 
 if [ -n "$TRAVIS_TAG" ]; then
-    mkdir -p build/${TRAVIS_TAG}
-    mv build/${APP_NAME}-linux build/${TRAVIS_TAG}/${APP_NAME}-${TRAVIS_TAG}-linux-amd64
-    mv build/${APP_NAME}-macos build/${TRAVIS_TAG}/${APP_NAME}-${TRAVIS_TAG}-darwin-amd64
-    mv build/${APP_NAME}-win.exe build/${TRAVIS_TAG}/${APP_NAME}-${TRAVIS_TAG}-windows-amd64.exe
+    mkdir -p ${BIN_PATH}
+    mv build/${APP_NAME}-linux ${BIN_PATH}/${APP_NAME}-${TRAVIS_TAG}-linux-amd64
+    mv build/${APP_NAME}-macos ${BIN_PATH}/${APP_NAME}-${TRAVIS_TAG}-darwin-amd64
+    mv build/${APP_NAME}-win.exe ${BIN_PATH}/${APP_NAME}-${TRAVIS_TAG}-windows-amd64.exe
 else
-    mkdir -p build/${TRAVIS_BUILD_NUMBER}
-    mv build/${APP_NAME}-linux build/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-linux-amd64
-    mv build/${APP_NAME}-macos build/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-darwin-amd64
-    mv build/${APP_NAME}-win.exe build/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-windows-amd64.exe
+    mkdir -p ${BIN_PATH}/master/${TRAVIS_BUILD_NUMBER}
+    mv build/${APP_NAME}-linux ${BIN_PATH}/master/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-linux-amd64
+    mv build/${APP_NAME}-macos ${BIN_PATH}/master/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-darwin-amd64
+    mv build/${APP_NAME}-win.exe ${BIN_PATH}/master/${TRAVIS_BUILD_NUMBER}/${APP_NAME}-${TRAVIS_BUILD_NUMBER}-windows-amd64.exe
 fi
